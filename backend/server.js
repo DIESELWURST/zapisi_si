948d49eef52ce31cfd415b5854dee041c636fef9
DIESELWURST <104293210+DIESELWURST@users.noreply.github.com>
2025-02-23 23:38:54 +0100

    Added endpoints for sign up verification and user creation and connected them to the front end

--- backend/server.js
+++ backend/server.js
@@ -10,23 +10,17 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-console.log('Environment Variables:');
-console.log('MYSQLHOST:', process.env.MYSQLHOST);
-console.log('MYSQLUSER:', process.env.MYSQLUSER);
-console.log('MYSQLPASSWORD:', process.env.MYSQLPASSWORD);
-console.log('MYSQLDATABASE:', process.env.MYSQLDATABASE);
-console.log('MYSQLPORT:', process.env.MYSQLPORT);
-console.log('PORT:', process.env.PORT);
+
 
 const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQLPORT // Ensure this matches your MySQL server port
+  port: process.env.MYSQLPORT
 });
 
-connection.connect((err) => {
+connection.connect(err => {
   if (err) {
     console.error('Error connecting to the database:', err);
     return;
@@ -34,16 +28,45 @@ connection.connect((err) => {
   console.log('Connected to the MySQL database.');
 });
 
-app.get('/api/test-connection', (req, res) => {
-  const query = 'Describe User;';
+// Ednpoind za preverjanje uporabniškega gesla
+app.get('/api/user-exists', (req, res) => {
+  const username = req.query.username;
+  if (!username) {
+    return res.status(400).json({ error: 'Username is required' });
+  }
 
-  connection.query(query, (err, results) => {
+  const query = 'SELECT * FROM users WHERE username = ?';
+
+  connection.query(query, [username], (err, results) => {
     if (err) {
       console.error('Error querying the database:', err);
-      res.status(500).json({ error: 'Internal server error', details: err });
-      return;
+      return res.status(500).json({ error: 'Internal server error', details: err });
     }
-    res.json({ message: 'Database connection successful', results });
+
+    if (results.length > 0) {
+      return res.json({ exists: true, message: '1' });
+    } else {
+      return res.json({ exists: false, message: '0  ' });
+    }
+  });
+});
+
+// Endpoint za dodajanje uporabnikov
+app.post('/api/add-user', (req, res) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    return res.status(400).json({ error: 'Username, email, and password are required' });
+  }
+
+  const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+
+  connection.query(query, [username, email, password], (err, results) => {
+    if (err) {
+      console.error('Error adding user to the database:', err);
+      return res.status(500).json({ error: 'Internal server error', details: err });
+    }
+
+    return res.json({ message: 'User added successfully' });
   });
 });
 
