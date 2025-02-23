9363305ae639e0f4049039742b4603d05ad3771a
DIESELWURST <104293210+DIESELWURST@users.noreply.github.com>
2025-02-24 00:03:25 +0100

    Corrected import paths  in components

--- frontend/components/rightBar.jsx
+++ frontend/components/rightBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import '../styles.css'; 
+import './styles.css'; 
 
 const RightBar = ({ pages, onNewPage, onSelectPage }) => {
   const [sidebarVisible, setSidebarVisible] = useState(false);
