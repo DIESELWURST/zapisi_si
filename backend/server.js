21c19d5f1e9ea011530c513c5756500843edbc6a
DIESELWURST <104293210+DIESELWURST@users.noreply.github.com>
2025-02-23 22:33:20 +0100

    Update environment variables and server.js for correct MySQL port

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  database: process.env.MYSQL_DATABASE,
   port: process.env.MYSQLPORT // Ensure this matches your MySQL server port
 });
 
