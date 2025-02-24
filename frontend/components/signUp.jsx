0f32f8dad22280d2e9717e9808a18985bf86599d
DIESELWURST <104293210+DIESELWURST@users.noreply.github.com>
2025-02-24 11:52:20 +0100

    Corrected import paths  in components

--- frontend/components/signUp.jsx
+++ frontend/components/signUp.jsx
@@ -103,13 +103,13 @@ const SignUp = () => {
           <label htmlFor="username">Username:</label> <br />
           <input
             type="text"name="username"id="username"val={username} onChange={(e)=> setUsername(e.target.value )} required/> <br />
-          {usernameError && <p cl sName="error">{userneError}</p>}
+          {usernameError && <p className="error">{usernameError}</p>}
 
           <label htmlFor="password">Password:</label> <br />
-          <input             type="password"             name="password"             id="password"             value={password}             onChange={(e) => setPassword(e.target.value)}             required          /> <br />
+          <input type="password" name="password" id="password" value={password} onChange={(e) => setPassword(e.target.value)} required/> <br />
           
           <label htmlFor="confirmPassword">Please enter password again:</label> <br />
-          <input             type="password"             name="confirmPassword"             id="confirmPassword"             value={confirmPassword}             onChange={(e) => setConfirmPassword(e.target.value)}             required          /> <br />
+          <input type="password" name="confirmPassword" id="confirmPassword" value={confirmPassword} onChange={(e) => setConfirmPassword(e.target.value)} required/> <br />
 
           <label htmlFor="email">Email:</label> <br />
           <input
