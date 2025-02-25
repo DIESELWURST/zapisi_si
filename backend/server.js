775b8a8ad00e85f4799da65eb9fda56048c7911d
DIESELWURST <104293210+DIESELWURST@users.noreply.github.com>
2025-02-25 08:29:38 +0100

    The start of the implementation of the sign in

--- backend/server.js
+++ backend/server.js
@@ -10,20 +10,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-console.log('Environment Variables:');
-console.log('MYSQLHOST:', process.env.MYSQLHOST);
-console.log('MYSQLUSER:', process.env.MYSQLUSER);
-console.log('MYSQLPASSWORD:', process.env.MYSQLPASSWORD);
-console.log('MYSQLDATABASE:', process.env.MYSQL_DATABASE);
-console.log('MYSQLPORT:', process.env.MYSQLPORT);
-console.log('PORT:', process.env.PORT);
 
 const connection = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQL_DATABASE,
-  port: process.env.MYSQLPORT // Ensure this matches your MySQL server port
+  port: process.env.MYSQLPORT 
 });
 
 connection.connect(err => {
@@ -100,6 +93,33 @@ app.post('/api/add-user', (req, res) => {
     return res.json({ message: 'User added successfully' });
   });
 });
+
+
+
+// Endpoint , ki pregleduje vpis uporabnika
+app.get('/api/check-credentials', (req, res) => {
+  const credentials = req.query.credentials;
+  const password = req.query.password;
+  if (!credentials || !password) {
+    return res.status(400).json({ error: 'Credentials and password are required' });
+  }
+
+  const query = 'SELECT * FROM User WHERE (username = ? OR email = ?) AND password = ?';
+
+  connection.query(query, [credentials, credentials, password], (err, results) => {
+    if (err) {
+      console.error('Error querying the database:', err);
+      return res.status(500).json({ error: 'Internal server error', details: err });
+    }
+
+    if (results.length > 0) {
+      return res.json({ exists: true, message: '1' });
+    } else {
+      return res.json({ exists: false, message: '0' });
+    }
+  });
+});
+
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
