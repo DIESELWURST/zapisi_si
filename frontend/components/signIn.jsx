0a13c6f415c4891bdcbfc7a6b25fbc3fa7f19b80
DIESELWURST <104293210+DIESELWURST@users.noreply.github.com>
2025-03-06 19:34:29 +0100

    Password reset

--- frontend/components/signIn.jsx
+++ frontend/components/signIn.jsx
@@ -23,14 +23,35 @@ const SignIn = ({ setIsAuthenticated, setUser }) => {
     }
   };
 
+  const sendOtp = async (email) => {
+    try {
+      const response = await fetch('https://backend-production-fbab.up.railway.app/api/send-otp', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+
+      if (response.ok) {
+        setOtpSent(true);
+        alert('OTP sent to your email');
+      } else {
+        const data = await response.json();
+        setCredsError(data.error);
+      }
+    } catch (error) {
+      console.error('Error sending OTP:', error);
+      setCredsError('An error occurred while sending the OTP');
+    }
+  };
+
   const handleSignIn = async (event) => {
     event.preventDefault();
 
     const result = await checkCreds(credentials, password);
     if (result.exists) {
-      setIsAuthenticated(true);
-      setUser(result.user); // Assuming the backend returns user information
-      navigate('/app');
+      sendOtp(result.user.email);
     } else if (result.error) {
       setCredsError(result.error);
     } else {
@@ -42,18 +63,19 @@ const SignIn = ({ setIsAuthenticated, setUser }) => {
     event.preventDefault();
 
     try {
-      const response = await fetch('https://backend-production-fbab.up.railway.app/api/verify-email', {
+      const response = await fetch('https://backend-production-fbab.up.railway.app/api/verify-otp', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ credentials, code: otp }),
+        body: JSON.stringify({ email: credentials, otp }),
       });
 
       if (response.ok) {
-        alert('Email verified successfully');
-        setOtp('');
-        setOtpError('');
+        const data = await response.json();
+        setIsAuthenticated(true);
+        setUser(data.user); // Assuming the backend returns user information
+        navigate('/app');
       } else {
         const data = await response.json();
         setOtpError(data.error);
